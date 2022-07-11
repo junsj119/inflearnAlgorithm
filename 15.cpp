c6989815819be4ec1ae70592357b249791d6f4a5
junsj119 <wjstjddud119@naver.com>
2022-07-12 02:52:09 +0900

    07/12

---
+++ 15.cpp
@@ -0,0 +1,44 @@
+//15. 소수의 개수
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, flag, cnt = 0;
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		flag = 1;
+		for (int j = 2; j <= sqrt(i); j++) {
+			if (i % j == 0) {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1) cnt++;
+	}
+	cout << cnt;
+
+	return 0;
+}
+
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	return 0;
+}
+*/
---
+++ 16.cpp
@@ -0,0 +1,58 @@
+//16. Anagram
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int a[53];
+int b[53];
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string str;
+	
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (isupper(str[i]))
+			a[str[i] - 65];
+		else
+			a[str[i] - 97 - 26];		//0~25 대문자 26~ 소문자
+	}
+
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		if (isupper(str[i]))
+			b[str[i] - 65];
+		else
+			b[str[i] - 97 - 26];		//0~25 대문자 26~ 소문자
+	}
+	for (int i = 0; i < 52; i++) {
+		if (a[i] != b[i]) {
+			cout << "NO";
+			exit(0);
+		}
+	}
+	cout << "YES";
+
+	return 0;
+}
+
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	return 0;
+}
+*/
