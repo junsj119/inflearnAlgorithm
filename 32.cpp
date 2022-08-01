fe2060c7b9d88d694f524bca86142ffb7487e4f9
junsj119 <wjstjddud119@naver.com>
2022-08-01 22:31:31 +0900

    08/01

---
+++ 32.cpp
@@ -0,0 +1,51 @@
+//선택 정렬
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int arr[101];
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, idx, tmp;  
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+		cin >> arr[i];
+	
+	for (int i = 0; i < n - 1; i++) {
+		idx = i;
+		for (int j = i + 1; j < n; j++) {
+			if (arr[j] < arr[idx])
+				idx = j;
+		}
+		tmp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = tmp;
+	}
+
+	for (int i = 0; i < n; i++)
+		cout << arr[i];
+	
+
+	return 0;
+}
+
+/*
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	return 0;
+}
+*/
